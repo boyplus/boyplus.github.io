ee74e8f84be27fb615201f4df3ca741b7fb63627
Mr.THANAPHON SOMBUNKAEO <62130500212@st.sit.kmutt.ac.th>
2021-02-24 14:51:42 +0700

    [UPDATE] get route query name

--- pages/project/[name].js
+++ pages/project/[name].js
@@ -1,51 +1,51 @@
 import { Fragment } from "react";
+import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
 import BlogLayout from "../../components/BlogLayout";
 import projectInfo from "../../utils/projectInfo";
 
-const Project = ({ name }) => {
+const Project = () => {
+  const router = useRouter();
+  const name = router.query.name;
   const data = projectInfo.find((el) => el.name == name);
+  const renderProject = () => {
+    if (!name) return null;
+    return (
+      <Fragment>
+        <div className="container">
+          <div>
+            <img src={data.home} width="100%"></img>
+          </div>
+          <div style={{ paddingLeft: "30px" }}>
+            <h1>{data.title}</h1>
+            <p style={{ paddingTop: "10px" }}>{data.description}</p>
+            <h2 style={{ paddingTop: "10px" }}>Technology Stack</h2>
+            {data.stacks.map((el) => {
+              return (
+                <div key={el}>
+                  <p>{el}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <style jsx>{`
+          .container {
+            padding-top: 20px;
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+          }
+        `}</style>
+      </Fragment>
+    );
+  };
   return (
     <Fragment>
       <Layout page="project">
-        <BlogLayout>
-          <div className="container">
-            <div>
-              <img src={data.home} width="100%"></img>
-            </div>
-            <div style={{ paddingLeft: "30px" }}>
-              <h1>{data.title}</h1>
-              <p style={{ paddingTop: "10px" }}>{data.description}</p>
-              <h2 style={{ paddingTop: "10px" }}>Technology Stack</h2>
-              {data.stacks.map((el) => {
-                return (
-                  <div>
-                    <p>{el}</p>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </BlogLayout>
+        <BlogLayout>{renderProject()}</BlogLayout>
       </Layout>
-      <style jsx>{`
-        .container {
-          padding-top: 20px;
-          display: grid;
-          grid-template-columns: repeat(2, 1fr);
-        }
-      `}</style>
     </Fragment>
   );
 };
-
-export async function getServerSideProps(ctx) {
-  try {
-    const name = ctx.query.name;
-    return { props: { name } };
-  } catch (err) {
-    return { props: { name: "" } };
-  }
-}
 
 export default Project;
